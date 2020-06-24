2e8cb45575f46e1a5f3d18d3b959082e23fe701e
hoyyun <1048163216@qq.com>
2020-06-24 16:22:02 +0800

    新建一个 .gitignore 文件

---
+++ note.js
@@ -0,0 +1,25 @@
+// 1）、cd : 改变目录。
+// 2）、cd . . 回退到上一个目录，直接cd进入默认目录
+// 3）、pwd : 显示当前所在的目录路径。
+// 4）、ls(ll): 都是列出当前目录中的所有文件，只不过ll(两个ll)列出的内容更为详细。
+// 5）、touch : 新建一个文件 如 touch index.js 就会在当前目录下新建一个index.js文件。
+// 6）、rm: 删除一个文件, rm index.js 就会把index.js文件删除。
+// 7）、mkdir: 新建一个目录,就是新建一个文件夹。
+// 8）、rm -r : 删除一个文件夹, rm -r src 删除src目录， 好像不能用通配符。
+// 9）、mv 移动文件, mv index.html src index.html 是我们要移动的文件, src 是目标文件夹,当然, 这样写,必须保证文件和目标文件夹在同一目录下。
+// 10）、reset 重新初始化终端/清屏。
+// 11）、clear 清屏。
+// 12）、history 查看命令历史。
+// 13）、help 帮助。
+// 14）、exit 退出。
+// 15）、#表示注释
+
+// git remote：查看远程库信息 
+// git remote -v：远程库详细信息
+// git branch -r ， git branch -a 查看远程分支
+// git push 将当前分支推送到远程对应的分支（若远程无对应分支，则推送无效） 
+// git push origin dev 将分支dev提交到远程origin/dev（远程没有则创建, 远程没有dev则创建） 
+// git branch –set-upstream branch-name origin/branch-name 建立本地分支和远程分支的关联
+// git checkout -b dev origin/dev 创建远程的origin/dev分支到本地
+
+12344556
